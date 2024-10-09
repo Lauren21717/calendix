039cab32e4f0aa4b3d07e34b0d7cbf874a1c843c
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-09 17:49:56 +0000

    Add dashboard Nav
    - Installed `clsx` package for better handling of conditional class names.
    - Updated the `RightNav` component to change the "About" link to point to "Dashboard" when a user is logged in.

---
+++ src/app/(site)/dashboard/event-types/page.tsx
@@ -0,0 +1,10 @@
+import DashboardNav from "@/app/components/DashboardNav";
+
+export default function EventTypesPage() {
+    return (
+        <div>
+            <DashboardNav />
+            hello, from event types
+        </div>
+    );
+}
---
+++ src/app/(site)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import DashboardNav from "@/app/components/DashboardNav"
+
+export default function DashboardPage() {
+    return (
+        <>
+            <DashboardNav />
+            <div>
+                booked events listed here...
+            </div>
+        </>
+    );
+}
