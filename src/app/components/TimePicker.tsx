9847b22724c0d46ca69468a0be341a6768cc9213
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-14 00:10:02 +0000

    Feat: Add date selection to TimePicker with improved styles
    - Added `selectedDay` state for date selection.
    - Enhanced styling for active, selected, and today's dates.
    - Disabled booking for past and interactive dates.

--- src/app/components/TimePicker.tsx
+++ src/app/components/TimePicker.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { weekdaysShortNames } from "@/libs/shared";
-import { BookingTimes } from "@/libs/types";
-import { addDays, addMonths, format, getDay, isLastDayOfMonth, subMonths } from "date-fns";
+import { BookingTimes, WeekdayName } from "@/libs/types";
+import clsx from "clsx";
+import { addDays, addMonths, format, getDay, isEqual, isFuture, isLastDayOfMonth, isToday, subMonths } from "date-fns";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 
@@ -10,6 +11,8 @@ export default function TimePicker({ bookingTimes }: { bookingTimes: BookingTime
     const [activeMonthDate, setActiveMonthDate] = useState(currentDate);
     const [activeMonthIndex, setActiveMonthIndex] = useState(activeMonthDate.getMonth());
     const [activeYear, setActiveYear] = useState(activeMonthDate.getFullYear());
+    const [selectedDay, setSelectedDay] = useState<null|Date>(null);
+
     const firstDayOfCurrentMonth = new Date(activeYear, activeMonthIndex, 1);
     const firstDayOfCurrentMonthWeekdayIndex = getDay(firstDayOfCurrentMonth);
     const emptyDaysCount = firstDayOfCurrentMonthWeekdayIndex === 0 ? 6 : firstDayOfCurrentMonthWeekdayIndex - 1;
@@ -39,9 +42,13 @@ export default function TimePicker({ bookingTimes }: { bookingTimes: BookingTime
         });
     }
 
+    function handleDayClick(day:Date) {
+        setSelectedDay(day);
+    }
+
     return (
         <div className="flex gap-4">
-            <div className="grow">
+            <div>
                 <div className="flex items-center">
                     <span className="grow">
                         {format(new Date(activeYear, activeMonthIndex), "MMMM")} {activeYear}:
@@ -63,16 +70,32 @@ export default function TimePicker({ bookingTimes }: { bookingTimes: BookingTime
                     {emptyDaysArr.map(empty => (
                         <div></div>
                     ))}
-                    {daysNumbers.map(n => (
-                        <div className="text-center text-sm text-gray-500 font-bold">
-                            <div className="bg-gray-300 w-8 h-8 rounded-full inline-flex items-center justify-center">
-                                {format(n, 'd')}
+                    {daysNumbers.map(n => {
+                        const weekdayNameIndex = format(n, "EEEE").toLowerCase() as WeekdayName;
+                        const weekdayConfig = bookingTimes?.[weekdayNameIndex];
+                        const isActiveInBookingTimes = weekdayConfig?.active;
+                        const canBeBooked = isFuture(n) && isActiveInBookingTimes;
+                        const isSelected = selectedDay && isEqual(n, selectedDay);
+
+                        return (
+                            <div className="text-center text-sm text-gray-400 font-bold">
+                                <button
+                                    disabled={!canBeBooked}
+                                    onClick={() => handleDayClick(n)}
+                                    className={clsx(
+                                        "w-8 h-8 rounded-full inline-flex items-center justify-center",
+                                        canBeBooked && !isSelected ? "bg-blue-200 text-blue-700" : "",
+                                        isSelected ? "bg-blue-500 text-white" : "",
+                                        isToday(n) && !isSelected ? "bg-gray-200 text-gray-500" : "",
+                                    )}
+                                >
+                                    {format(n, 'd')}
+                                </button>
                             </div>
-                        </div>
-                    ))}
+                        );
+                    })}
                 </div>
             </div>
-            
             <div className="border border-black">
                 time buttons
             </div>
